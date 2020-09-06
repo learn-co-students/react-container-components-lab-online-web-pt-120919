7b448706bc9b02700c387b6df6aa82516230860b
Mely07 <damely.tineo07@gmail.com>
2020-09-05 22:44:29 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+ 
+const MovieReviews = ({reviews}) => {
+   return (
+       <div className = 'review-list'>
+           
+               {reviews.map((review, index) => <li className = 'review' key={index}> {review.display_title} - {review.summary_short}</li>)}
+          
+       </div>
+   );
+}
+
+export default MovieReviews;
