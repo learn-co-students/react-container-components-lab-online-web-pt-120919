0206f0e6badc59a3febf5d84e7dc5f0795f2be60
Charity Parks <tuppercharity@gmail.com>
2020-09-08 20:19:02 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,35 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        reviews: []
+      };
+    }
+  
+    fetchReviews = () => {
+      fetch(URL)
+      .then(results => results.json())
+      .then(reviews => {
+        this.setState({reviews: reviews.results})
+      })
+    };
+  
+    componentDidMount(){
+      this.fetchReviews();
+    }
+  
+    render() {
+      return (
+          <div className="latest-movie-reviews">
+            <h1>Latest reviews</h1>
+            <MovieReviews reviews={this.state.reviews}/>
+          </div>
+      );
+    }
+  }
+  
+  export default LatestMovieReviewsContainer;
